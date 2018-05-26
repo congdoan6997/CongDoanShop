4fe57ec3df4c978609e3e4de49c44cd095310091
Congdoan97 <congdoan6997@gmail.com>
2018-05-26 15:34:40 +0300

    Revert "Revert "fix post repository and add post category , post service""
    This reverts commit 1460e7d9872d8d81789ebbaa0774a19d542a29f1.

---
+++ CongDoan.Service/PostCategoryService.cs
@@ -0,0 +1,72 @@
+﻿using CongDoan.Data.Infacstructure;
+using CongDoan.Data.Repositories;
+using CongDoan.Model.Models;
+using System.Collections.Generic;
+
+namespace CongDoan.Service
+{
+    public interface IPostCategoryService
+    {
+        PostCategory Add(PostCategory postCategory);
+
+        PostCategory Delete(int id);
+
+        void Update(PostCategory postCategory);
+
+        IEnumerable<PostCategory> GetAll();
+
+        IEnumerable<PostCategory> GetAllByParentId(int parentId);
+
+        PostCategory GetById(int id);
+
+        void SaveChange();
+    }
+
+    public class PostCategoryService : IPostCategoryService
+    {
+        private IPostCategoryRepository _postCategoryRepository;
+        private IUnitOfWork _unitOfWork;
+
+        public PostCategoryService(IPostCategoryRepository postCategoryRepository, IUnitOfWork unitOfWork)
+        {
+            this._postCategoryRepository = postCategoryRepository;
+            this._unitOfWork = unitOfWork;
+        }
+
+        public PostCategory Add(PostCategory postCategory)
+        {
+            return this._postCategoryRepository.Add(postCategory);
+        }
+
+        public PostCategory Delete(int id)
+        {
+            return this._postCategoryRepository.Delete(id);
+        }
+
+        public IEnumerable<PostCategory> GetAll()
+        {
+            return this._postCategoryRepository.GetAll();
+        }
+
+        public IEnumerable<PostCategory> GetAllByParentId(int parentId)
+        {
+            return this._postCategoryRepository.GetMulti(x => x.ParentID == parentId && x.Status);
+        }
+
+        public PostCategory GetById(int id)
+        {
+            return this._postCategoryRepository.GetSingleById(id);
+        }
+
+        public void SaveChange()
+        {
+            this._unitOfWork.Commit();
+        }
+
+        public void Update(PostCategory postCategory)
+        {
+            this._postCategoryRepository.Update(postCategory);
+            this._postCategoryRepository.Update(postCategory);
+        }
+    }
+}
