1460e7d9872d8d81789ebbaa0774a19d542a29f1
Congdoan97 <congdoan6997@gmail.com>
2018-05-26 15:29:24 +0300

    Revert "fix post repository and add post category , post service"
    This reverts commit 0f6369fe0c75bd51d542f8af7c5d78c833c07bc0.

--- CongDoan.Data/Infacstructure/RepositoryBase.cs
+++ CongDoan.Data/Infacstructure/RepositoryBase.cs
@@ -3,53 +3,53 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace CongDoan.Data.Infacstructure
 {
     public abstract class RepositoryBase<T> : IRepository<T> where T : class
     {
         #region Properties
-
         private CongDoanShopDbContext _dbContext;
         private readonly IDbSet<T> _dbSet;
         protected IDbFactory DbFactory { get; private set; }
         protected CongDoanShopDbContext DbContext => this._dbContext ?? (_dbContext = DbFactory.Init());
 
-        #endregion Properties
+        #endregion
 
         public RepositoryBase(IDbFactory dbFactory)
         {
             this.DbFactory = dbFactory;
             this._dbSet = DbContext.Set<T>();
         }
-
-        public virtual T Add(T entity)
+        public T Add(T entity)
         {
             return this._dbSet.Add(entity);
         }
 
-        public virtual bool CheckContains(Expression<Func<T, bool>> predicate)
+        public bool CheckContains(Expression<Func<T, bool>> predicate)
         {
             return this._dbSet.Count(predicate) > 0;
         }
 
-        public virtual int Count(Expression<Func<T, bool>> where)
+        public int Count(Expression<Func<T, bool>> where)
         {
             return this._dbSet.Count(where);
         }
 
-        public virtual T Delete(T entity)
+        public T Delete(T entity)
         {
             return this._dbSet.Remove(entity);
         }
 
-        public virtual T Delete(int id)
+        public T Delete(int id)
         {
             var entity = _dbSet.Find(id);
             return this._dbSet.Remove(entity);
         }
 
-        public virtual void DeleteMulti(Expression<Func<T, bool>> expression)
+        public void DeleteMulti(Expression<Func<T, bool>> expression)
         {
             var objects = _dbSet.Where(expression).AsEnumerable();
             foreach (var item in objects)
@@ -58,7 +58,7 @@ namespace CongDoan.Data.Infacstructure
             }
         }
 
-        public virtual IEnumerable<T> GetAll(string[] includes = null)
+        public IEnumerable<T> GetAll(string[] includes = null)
         {
             if (includes != null && includes.Count() > 0)
             {
@@ -72,12 +72,12 @@ namespace CongDoan.Data.Infacstructure
             return _dbSet.AsEnumerable();
         }
 
-        public virtual IEnumerable<T> GetMany(Expression<Func<T, bool>> where, string[] includes = null)
+        public IEnumerable<T> GetMany(Expression<Func<T, bool>> where, string[] includes = null)
         {
             return _dbSet.Where(where).AsEnumerable();
         }
 
-        public virtual IEnumerable<T> GetMulti(Expression<Func<T, bool>> predicate, string[] includes = null)
+        public IEnumerable<T> GetMulti(Expression<Func<T, bool>> predicate, string[] includes = null)
         {
             if (includes != null && includes.Count() > 0)
             {
@@ -91,7 +91,7 @@ namespace CongDoan.Data.Infacstructure
             return _dbSet.Where(predicate).AsEnumerable();
         }
 
-        public virtual IEnumerable<T> GetMultiPaging(Expression<Func<T, bool>> filter, out int total, int index = 0, int size = 50, string[] includes = null)
+        public IEnumerable<T> GetMultiPaging(Expression<Func<T, bool>> filter, out int total, int index = 0, int size = 50, string[] includes = null)
         {
             int skipCount = index * size;
             IEnumerable<T> _resetSet;
@@ -113,7 +113,7 @@ namespace CongDoan.Data.Infacstructure
             return _resetSet.AsEnumerable();
         }
 
-        public virtual T GetSingleByCondition(Expression<Func<T, bool>> expression, string[] includes = null)
+        public T GetSingleByCondition(Expression<Func<T, bool>> expression, string[] includes = null)
         {
             if (includes != null && includes.Count() > 0)
             {
@@ -127,12 +127,12 @@ namespace CongDoan.Data.Infacstructure
             return _dbSet.FirstOrDefault(expression);
         }
 
-        public virtual T GetSingleById(int id)
+        public T GetSingleById(int id)
         {
             return this._dbSet.Find(id);
         }
 
-        public virtual void Update(T entity)
+        public void Update(T entity)
         {
             _dbSet.Attach(entity);
             _dbContext.Entry(entity).State = EntityState.Modified;
